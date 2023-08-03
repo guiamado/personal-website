9b5f7fbc83080344c6eff25b27df0fc4bbec2932
guiamado <amado.silva133@gmail.com>
2023-08-03 19:09:36 -0300

    Fix the scrolling in bigger screen

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="bg-primary leading-relaxed text-slate-400 antialiased selection:bg-[#5b008f] selection:text-white min-h-screen relative">
     <router-view></router-view>
-    
   </div>
 </template>
